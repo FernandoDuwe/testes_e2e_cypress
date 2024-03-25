ad49ffaa7da005e95d49c0bda2ff297ece7e5d90
Duwe <fernando_duwe@hotmail.com>
2024-03-24 23:00:57 -0300

    CRUD

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
 	cy.intercept('GET', '**/notes').as('getNotes')
 	cy.visit('/signup')
@@ -61,4 +35,32 @@ Cypress.Commands.add('sessionLogin', (
 ) => {
 	const login = () => cy.guiLogin(username, password)
 	cy.session(username, login)
+})
+
+// CRUD
+Cypress.Commands.add('createNote', (note, password = false) => {
+	cy.get('#content').type(note)
+	cy.contains('button', 'Create').click()
+
+	if (password) {
+		cy.get('#file').selectFile('cypress/fixtures/example.json')
+	}
+
+	cy.wait('@getNotes')
+	cy.contains('.list-group-item', note)
+		.should('be.visible')
+})
+
+Cypress.Commands.add('updateNote', (note, attachFile = false) => {
+	cy.get('#content')
+		.as('contentField')
+		.clear()
+	cy.get('@contentField')
+		.type(note)
+
+	if (attachFile) {
+		cy.get('#file').selectFile('cypress/fixtures/example.json')
+	}
+
+	cy.contains('button', 'Save').click()
 })
